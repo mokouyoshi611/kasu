a0caf3bd8f85cc2764641aff1c6f23796e9c263e
Okayama Tomoki <tomoki.okayama.04@gmail.com>
2024-07-02 15:36:44 +0900

    gitignore最高神

--- 熊を目撃＆出没した場所を表示するアプリ/熊を目撃＆出没した場所を表示するアプリ/obj/Debug/net8.0-windows/熊を目撃＆出没した場所を表示するアプリ.AssemblyInfo.cs
+++ 熊を目撃＆出没した場所を表示するアプリ/熊を目撃＆出没した場所を表示するアプリ/obj/Debug/net8.0-windows/熊を目撃＆出没した場所を表示するアプリ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("熊を目撃＆出没した場所を表示するアプリ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb018766cc51c00525862148471e0627f008879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b7a1ad2cf13219e2e8dd4f7b6d6d572089bd1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("熊を目撃＆出没した場所を表示するアプリ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("熊を目撃＆出没した場所を表示するアプリ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
