265915db5ef443e309b6532692ae5e7ad35c7ce5
mok <jojy0628@gmail.com>
2024-07-02 13:45:03 +0900

    dsgnfou4nh

---
+++ 熊を目撃＆出没した場所を表示するアプリ/熊を目撃＆出没した場所を表示するアプリ/Form1.cs
@@ -0,0 +1,10 @@
+namespace 熊を目撃_出没した場所を表示するアプリ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 熊を目撃＆出没した場所を表示するアプリ/熊を目撃＆出没した場所を表示するアプリ/Program.cs
@@ -0,0 +1,17 @@
+namespace 熊を目撃_出没した場所を表示するアプリ
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
