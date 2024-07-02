980e9fb3bf48e12e7ec4c95f0a8f6940de1a1007
Okayama Tomoki <tomoki.okayama.04@gmail.com>
2024-07-02 15:55:48 +0900

    gitignore更新

--- 熊を目撃＆出没した場所を表示するアプリ/熊を目撃＆出没した場所を表示するアプリ/Form1.cs
+++ 熊を目撃＆出没した場所を表示するアプリ/熊を目撃＆出没した場所を表示するアプリ/Form1.cs
@@ -3,6 +3,7 @@ namespace 熊を目撃_出没した場所を表示するアプリ
     public partial class Form1 : Form
     {
         int a = 0;
+        int b = 2;
         public Form1()
         {
             InitializeComponent();
