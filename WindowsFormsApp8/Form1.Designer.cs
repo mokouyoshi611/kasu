6ac89ab89ca3814ab63724f080f421b950a5c5cc
mok <jojy0628@gmail.com>
2024-07-16 13:44:57 +0900

    yosiko

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.webBrowser1.Location = new System.Drawing.Point(449, -1);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(556, 640);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(81, 221);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(316, 19);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(208, 455);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 635);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        private const string AzureMapsApiKey = "AIzaSyBxbmsffMgblAR26tM26UbkO3o-vodxfvo";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("\"C:\\Users\\yoshi\\source\\repos\\WindowsFormsApp8\\WindowsFormsApp8\\HTMLPage1.html\"");
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string address = txtAddress.Text;
+            if (!string.IsNullOrEmpty(address))
+            {
+                var location = await GetCoordinatesAsync(address);
+                if (location != null)
+                {
+                    AddPinToMap(location.Item1, location.Item2);
+                    SavePinInformation(address, location.Item1, location.Item2);
+                }
+                else
+                {
+                    MessageBox.Show("住所の検索に失敗しました。");
+                }
+            }
+        }
+        private async Task<Tuple<double, double>> GetCoordinatesAsync(string address)
+        {
+            string url = $"https://atlas.microsoft.com/search/address/json?api-version=1.0&subscription-key={AzureMapsApiKey}&query={Uri.EscapeDataString(address)}";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(content);
+                    var position = json["results"]?[0]?["position"];
+                    if (position != null)
+                    {
+                        double latitude = (double)position["lat"];
+                        double longitude = (double)position["lon"];
+                        return Tuple.Create(latitude, longitude);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void AddPinToMap(double latitude, double longitude)
+        {
+            string script = $"addPin({latitude}, {longitude});";
+            webBrowser1.Document.InvokeScript("eval", new object[] { script });
+        }
+
+        private void SavePinInformation(string address, double latitude, double longitude)
+        {
+            string filePath = "pins.txt";
+            string pinInfo = $"{address},{latitude},{longitude}{Environment.NewLine}";
+            File.AppendAllText(filePath, pinInfo);
+        }
+
+
+    }
+}
+
